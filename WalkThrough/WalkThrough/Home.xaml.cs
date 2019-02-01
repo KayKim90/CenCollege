4211a004c8f5c785d617baea441831895d5e3d0b
KayKim90 <44704644+KayKim90@users.noreply.github.com>
2019-02-01 17:35:13 -0500

    Q4. Walk through(binding)

---
+++ WalkThrough/WalkThrough/Home.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WalkThrough
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnViewDetail(object sender, RoutedEventArgs e)
+        {
+            Detail test = new Detail();
+            this.NavigationService.Navigate(test);
+            this.NavigationService.Navigate(test);
+        }
+    }
+}
